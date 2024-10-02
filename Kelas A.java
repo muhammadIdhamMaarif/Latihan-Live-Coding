e31b0bdebb6c434f6cd84a76fa515a6026f422db
muhammadIdhamMaarif <muhammadidhammaarif@gmail.com>
2024-10-02 21:45:22 +0700

    Update Kelas A.java

--- Kelas A.java
+++ Kelas A.java
@@ -1,6 +1,4 @@
-    import java.time.Duration;
-    import java.time.LocalDateTime;
-    import java.time.format.DateTimeFormatter;
+
 
     /*
     ==================================
@@ -12,29 +10,62 @@
 
      */
 
-    public class Main {
+import java.util.Scanner;
 
-        public static void main(String[] args) {
-            //ini sampel input yang ntar dimasukin ke parseDateTime
-            String inputA = "30 aug 14 59";
-            String inputB = "2 sep 18 32";
+public class Main {
 
-            LocalDateTime dateTimeA = parseDateTime(inputA);
-            LocalDateTime dateTimeB = parseDateTime(inputB);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int hariA = scanner.nextInt();
+        String bulanA = scanner.next();
+        int jamA = scanner.nextInt();
+        int menitA = scanner.nextInt();
+        
+        int hariB = scanner.nextInt();
+        String bulanB = scanner.next();
+        int jamB = scanner.nextInt();
+        int menitB = scanner.nextInt();
+        
+        int daysA = convertToDays(hariA, bulanA);
+        int daysB = convertToDays(hariB, bulanB);
+        
+        int totalMinutesA = (daysA * 24 * 60) + (jamA * 60) + menitA;
+        int totalMinutesB = (daysB * 24 * 60) + (jamB * 60) + menitB;
+        
+        int diffMinutes = totalMinutesB - totalMinutesA;
+        
+        int diffDays = diffMinutes / (24 * 60);
+        diffMinutes %= (24 * 60);
 
-            Duration duration = Duration.between(dateTimeA, dateTimeB);
+        int diffHours = diffMinutes / 60;
+        diffMinutes %= 60;
+        
+        System.out.printf("%dd %dh %dm%n", diffDays, diffHours, diffMinutes);
 
-            long days = duration.toDays();
-            duration = duration.minusDays(days);
-            long hours = duration.toHours();
-            duration = duration.minusHours(hours);
-            long minutes = duration.toMinutes();
-
-            System.out.printf("%dd %dh %dm%n", days, hours, minutes);
-        }
-
-        private static LocalDateTime parseDateTime(String input) {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM HH mm");
-            return LocalDateTime.parse(input + " 2024", formatter.withLocale(java.util.Locale.ENGLISH));
-        }
+        scanner.close();
     }
+    
+    public static int convertToDays(int day, String month) {
+        int[] daysInMonths = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        String[] monthNames = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
+        int monthIndex = 0;
+        
+        for (int i = 0; i < monthNames.length; i++) {
+            if (month.equalsIgnoreCase(monthNames[i])) {
+                monthIndex = i;
+                break;
+            }
+        }
+        
+        int totalDays = 0;
+        for (int i = 0; i < monthIndex; i++) {
+            totalDays += daysInMonths[i];
+        }
+        
+        totalDays += day;
+
+        return totalDays;
+    }
+}
